7c5dea52a98442a3cf3fa4d3dc5aed2520090fbb
Cedar Chavez <geggas00@gmail.com>
2022-09-09 23:52:27 -0700

    pulled in fsUtils library, and pulled in base for routes from reference

--- Develop/server.js
+++ Develop/server.js
@@ -2,8 +2,8 @@
 const express = require('express');
 //calling in path? whats that
 const path = require('path');
-//calling API, but should it be the database instead
-// const api = require('./public/assets/js/index');
+//calling API to direct users requests
+const api = require('./routes/index');
 
 //no idea what this is doing, something to do with the server
 const PORT = process.env.PORT || 3002;
@@ -14,11 +14,13 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
+//used for API request
+app.use('/api', api);
 
 app.use(express.static('public'));
 
 // GET Route for homepage
+//used for page location
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
@@ -28,7 +30,7 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-
+//where the local host can be viewed?
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
