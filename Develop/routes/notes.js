69f537f46ec43c960f8736c8b73946ae1c633e91
Cedar Chavez <geggas00@gmail.com>
2022-09-10 16:52:36 -0700

    update API router for notes to display saved, pull individual notes, delete notes, and create a new note

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,67 +1,64 @@
 const notes = require('express').Router();
 const uuid = require('../helpers/uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
 
-// const { v4: uuidv4 } = require('uuid');
-// const {
-//   readFromFile,
-//   readAndAppend,
-//   writeToFile,
-// } = require('../helpers/fsUtils');
-
-// GET Route for retrieving all the tips
+// GET Route for retrieving all notes from JSON
 notes.get('/', (req, res) => {
-  readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// GET Route for a specific tip
-tips.get('/:tip_id', (req, res) => {
-  const tipId = req.params.tip_id;
-  readFromFile('./db/tips.json')
+// GET Route for a specific note
+notes.get('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
     .then((data) => JSON.parse(data))
     .then((json) => {
-      const result = json.filter((tip) => tip.tip_id === tipId);
+      const result = json.filter((note) => note.id === noteId);
       return result.length > 0
         ? res.json(result)
-        : res.json('No tip with that ID');
+        : res.json('No note with that ID');
     });
 });
 
-// DELETE Route for a specific tip
-tips.delete('/:tip_id', (req, res) => {
-  const tipId = req.params.tip_id;
-  readFromFile('./db/tips.json')
+// DELETE Route for a specific note
+notes.delete('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
     .then((data) => JSON.parse(data))
     .then((json) => {
       // Make a new array of all tips except the one with the ID provided in the URL
-      const result = json.filter((tip) => tip.tip_id !== tipId);
+      const result = json.filter((note) => note.id !== noteId);
 
       // Save that array to the filesystem
-      writeToFile('./db/tips.json', result);
+      writeToFile('./db/db.json', result);
 
       // Respond to the DELETE request
-      res.json(`Item ${tipId} has been deleted 🗑️`);
+      res.json(`Item ${noteId} has been deleted 🗑️`);
     });
 });
 
-// POST Route for a new UX/UI tip
-tips.post('/', (req, res) => {
+// POST Route for a new note
+notes.post('/', (req, res) => {
   console.log(req.body);
 
-  const { username, topic, tip } = req.body;
+  const { title, text } = req.body;
 
   if (req.body) {
-    const newTip = {
-      username,
-      tip,
-      topic,
-      tip_id: uuidv4(),
+    const newNote = {
+      title,
+      text,
+      tip_id: uuid(),
     };
 
-    readAndAppend(newTip, './db/tips.json');
-    res.json(`Tip added successfully 🚀`);
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
   } else {
-    res.error('Error in adding tip');
+    res.error('Error in adding Note');
   }
 });
 
-module.exports = tips;
+module.exports = notes;
